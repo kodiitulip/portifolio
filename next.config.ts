a35aa7281f4dbc80a5dd25cbad45c6553b7d7d48
kodiitulip <kodii.tulip@gmail.com>
2025-12-30 06:04:10 -0300

    fix: typo

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: 'portifolio',
+  basePath: '/portifolio',
   images: {
     unoptimized: true
   }
